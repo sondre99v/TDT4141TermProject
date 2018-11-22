8fcf10b671459b698eae3a26c876fee880f6cbd9
Sondre Ninive Andersen <sondreixix@gmail.com>
2018-11-22 10:06:49 +0100

    Use explicit single-adder implementation in mod_mult to reduce LUT utilization

--- edit_rsa_accelerator/edit_rsa_accelerator.srcs/sources_1/new/mod_mult.vhd
+++ edit_rsa_accelerator/edit_rsa_accelerator.srcs/sources_1/new/mod_mult.vhd
@@ -45,9 +45,29 @@ architecture rtl of mod_mult is
 	signal FactorB : unsigned(WORD_WIDTH downto 0);
 	signal Modulus : unsigned(WORD_WIDTH-1 downto 0);
 	signal Counter : unsigned(integer(ceil(log2(real(WORD_WIDTH)))) downto 0);
-
+    
+    signal AdderResult : unsigned(WORD_WIDTH downto 0);
+    signal AdderInputB : unsigned(WORD_WIDTH downto 0);
+    signal AdderCarryIn : unsigned(0 downto 0);
 begin
-	process(clk, reset_n, State)
+    AdderResult <= Product + AdderInputB + AdderCarryIn;
+    
+    process(State, Modulus, FactorA) begin
+        if (State = ShiftProduct or State = Accumulate) then
+            -- Setup adder to subtract the modulus
+            AdderInputB <= not unsigned('0' & Modulus);
+            AdderCarryIn <= to_unsigned(1, 1);
+        elsif (State = FirstReduction) then
+            -- Setup adder to add factor A
+            AdderInputB <= unsigned('0' & FactorA);
+            AdderCarryIn <= to_unsigned(0, 1);
+        else
+            AdderInputB <= (others => '0');
+            AdderCarryIn <= (others => '0');
+        end if;
+    end process;
+    
+	process(clk, reset_n)
         --Variables inside processes
         variable nextState : State_Type;
         variable temp : unsigned(WORD_WIDTH downto 0) := (others => '0');
@@ -117,20 +137,27 @@ begin
 					
 				when FirstReduction =>
 					FactorB <= shift_left(FactorB, 1);
-					temp := Product - unsigned('0' & Modulus);
-					if (temp(WORD_WIDTH) = '0') then
-						Product <= temp;
+					--temp := Product - unsigned('0' & Modulus);
+					--if (temp(WORD_WIDTH) = '0') then
+					--	Product <= temp;
+					--end if;
+					if (AdderResult(WORD_WIDTH) = '0') then
+					   Product <= AdderResult;
 					end if;
 				    
 				when Accumulate =>
-					Product <= Product + unsigned('0' & FactorA);
-				
+					--Product <= Product + unsigned('0' & FactorA);
+				    Product <= AdderResult;
+				    
 				when SecondReduction =>
-					temp := Product - unsigned('0' & Modulus);
-					if (temp(WORD_WIDTH) = '0') then
-						Product <= temp;
-					end if;
-				
+					--temp := Product - unsigned('0' & Modulus);
+					--if (temp(WORD_WIDTH) = '0') then
+					--	Product <= temp;
+					--end if;
+				    
+                    if (AdderResult(WORD_WIDTH) = '0') then
+                       Product <= AdderResult;
+                    end if;
 			end case;
 		end if;
 		
